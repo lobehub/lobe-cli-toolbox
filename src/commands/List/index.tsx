aedf77dd4696d007e4d333919e47065ce81907c9
canisminor1990 <i@canisminor.cc>
2023-05-15 22:14:31 +0800

    chore: update

--- src/commands/List/index.tsx
+++ src/commands/List/index.tsx
@@ -1,20 +1,23 @@
 import { Box, Text } from 'ink';
 import React from 'react';
-import gitmojis from '../../constants/gitmojis-lite.json';
+import gitmojis from '../../constants/gitmojis-lite';
 
-const List: React.FC = () =>
-  gitmojis.map((item) => (
-    <Box key={item.name}>
-      <Box>
-        <Text>{item.emoji}</Text>
-        <Text>{Array.from({ length: item.emoji.length + 1 }).join('1')}</Text>
-      </Box>
+const List: React.FC = () => (
+  <>
+    {gitmojis.map((item) => (
+      <Box key={item.name}>
+        <Box>
+          <Text>{item.emoji}</Text>
+          <Text>{Array.from({ length: item.emoji.length + 1 }).join('1')}</Text>
+        </Box>
 
-      <Box width={16}>
-        <Text>{item.description.split(': ')[0]}</Text>
+        <Box width={16}>
+          <Text>{item.description.split(': ')[0]}</Text>
+        </Box>
+        <Text>{item.description.split(': ')[1]}</Text>
       </Box>
-      <Text>{item.description.split(': ')[1]}</Text>
-    </Box>
-  ));
+    ))}
+  </>
+);
 
 export default List;
