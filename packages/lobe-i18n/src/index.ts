2513ce1f17884c60e430f5350fb65230b3b11aad
canisminor1990 <i@canisminor.cc>
2023-11-30 10:55:11 +0800

    🐛 fix: Fix i18n defineConfig

--- packages/lobe-i18n/src/index.ts
+++ packages/lobe-i18n/src/index.ts
@@ -1,10 +1,6 @@
-import { merge } from 'lodash-es';
-
-import { DEFAULT_CONFIG } from '@/store/initialState';
-
 import type { I18nConfig } from './types/config';
 
 export type Config = I18nConfig;
 export const defineConfig = (config: Partial<Config>): Config => {
-  return merge(config, DEFAULT_CONFIG) as Config;
+  return config as Config;
 };
