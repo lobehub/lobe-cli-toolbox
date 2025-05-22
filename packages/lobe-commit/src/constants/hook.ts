ad5b7cd4480b71960ab2b2e593f5f8c211973817
canisminor1990 <i@canisminor.cc>
2025-05-22 13:34:41 +0800

    🐛 fix: Fix hook in wsl

--- packages/lobe-commit/src/constants/hook.ts
+++ packages/lobe-commit/src/constants/hook.ts
@@ -1,12 +1,14 @@
 export const HOOK = {
-  CONTENTS:
-    '#!/usr/bin/env sh\n# lobe-commit as a commit hook\n' +
-    'if npx -v >&/dev/null\n' +
-    'then\n' +
-    '  exec < /dev/tty\n  npx -c "lobe-commit --hook $1 $2"\n' +
-    'else\n' +
-    '  exec < /dev/tty\n  lobe-commit --hook $1 $2\n' +
-    'fi',
+  CONTENTS: `#!/usr/bin/env sh
+# lobe-commit as a commit hook
+if npx -v >/dev/null 2>&1
+then
+  exec < /dev/tty
+  npx -c "lobe-commit --hook $1 $2"
+else
+  exec < /dev/tty
+  lobe-commit --hook $1 $2
+fi`,
   FILENAME: 'prepare-commit-msg',
   PERMISSIONS: 0o775,
 };
