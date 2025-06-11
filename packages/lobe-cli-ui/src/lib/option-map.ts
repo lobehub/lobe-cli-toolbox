2da506b9cf0b4ff6f0c1978a511ff60b44eb0265
canisminor1990 <i@canisminor.cc>
2025-06-11 13:46:41 +0800

    🐛 fix: fix gitignore

---
+++ packages/lobe-cli-ui/src/lib/option-map.ts
@@ -0,0 +1,40 @@
+import {type Option} from '../types';
+
+type OptionMapItem = Option & {
+	previous: OptionMapItem | undefined;
+	next: OptionMapItem | undefined;
+	index: number;
+};
+
+export default class OptionMap extends Map<string, OptionMapItem> {
+	readonly first: OptionMapItem | undefined;
+
+	constructor(options: Option[]) {
+		const items: Array<[string, OptionMapItem]> = [];
+		let firstItem: OptionMapItem | undefined;
+		let previous: OptionMapItem | undefined;
+		let index = 0;
+
+		for (const option of options) {
+			const item = {
+				...option,
+				previous,
+				next: undefined,
+				index,
+			};
+
+			if (previous) {
+				previous.next = item;
+			}
+
+			firstItem ||= item;
+
+			items.push([option.value, item]);
+			index++;
+			previous = item;
+		}
+
+		super(items);
+		this.first = firstItem;
+	}
+}
